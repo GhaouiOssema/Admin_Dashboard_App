a5782d6494c518a77b5aa69cbdd11218362b4fe7
oussema <oghawi220@gmail.com>
2022-09-26 21:25:07 +0100

    Charts && Cart

--- src/components/ChartsHeader.jsx
+++ src/components/ChartsHeader.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const ChartsHeader = () => {
-	return <div>ChartsHeader</div>;
-};
+const ChartsHeader = ({ category, title }) => (
+	<div className='mb-10'>
+		<p className='text-gray-400 dark:text-gray-400'>{category}</p>
+		<p className='dark:text-gray-200 text-3xl font-extrabold tracking-tight text-slate-900'>
+			{title}
+		</p>
+	</div>
+);
 
 export default ChartsHeader;
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -3,49 +3,53 @@ import React, { createContext, useContext, useState } from 'react';
 const StateContext = createContext();
 
 const initialState = {
-	cart: false,
 	chat: false,
+	cart: false,
 	userProfile: false,
 	notification: false,
 };
 
 export const ContextProvider = ({ children }) => {
-	const [activeMenu, setActiveMenu] = useState(true);
-	const [isClicked, setIsClicked] = useState(initialState);
 	const [screenSize, setScreenSize] = useState(undefined);
 	const [currentColor, setCurrentColor] = useState('#03C9D7');
 	const [currentMode, setCurrentMode] = useState('Light');
 	const [themeSettings, setThemeSettings] = useState(false);
-	const handleClick = (clicked) => {
-		setIsClicked({ ...initialState, [clicked]: true });
-	};
+	const [activeMenu, setActiveMenu] = useState(true);
+	const [isClicked, setIsClicked] = useState(initialState);
+
 	const setMode = (e) => {
 		setCurrentMode(e.target.value);
 		localStorage.setItem('themeMode', e.target.value);
-		setThemeSettings(false);
 	};
+
 	const setColor = (color) => {
 		setCurrentColor(color);
 		localStorage.setItem('colorMode', color);
-		setThemeSettings(false);
 	};
 
+	const handleClick = (clicked) =>
+		setIsClicked({ ...initialState, [clicked]: true });
+
 	return (
+		// eslint-disable-next-line react/jsx-no-constructed-context-values
 		<StateContext.Provider
 			value={{
-				themeSettings,
-				setThemeSettings,
-				activeMenu,
-				setColor,
-				setMode,
-				setActiveMenu,
 				currentColor,
 				currentMode,
-				isClicked,
-				setIsClicked,
-				handleClick,
+				activeMenu,
 				screenSize,
 				setScreenSize,
+				handleClick,
+				isClicked,
+				initialState,
+				setIsClicked,
+				setActiveMenu,
+				setCurrentColor,
+				setCurrentMode,
+				setMode,
+				setColor,
+				themeSettings,
+				setThemeSettings,
 			}}>
 			{children}
 		</StateContext.Provider>
